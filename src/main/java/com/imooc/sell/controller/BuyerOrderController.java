f269ec6535918e28c1dff9879297b761553eca33
Citrix <sijieqian@foxmail.com>
2018-05-06 15:10:37 +0800

    56买家订单Service查询取消finish和paid

---
+++ src/main/java/com/imooc/sell/controller/BuyerOrderController.java
@@ -0,0 +1,14 @@
+package com.imooc.sell.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Citrix on 2018/5/6.
+ */
+@RestController
+@RequestMapping
+@Slf4j
+public class BuyerOrderController {
+}
