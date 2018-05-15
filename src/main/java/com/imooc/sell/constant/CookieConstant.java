41f497284291f4faf0a8f0167491cddd565ae745
Citrix <sijieqian@foxmail.com>
2018-05-15 15:26:46 +0800

    515登录登出

---
+++ src/main/java/com/imooc/sell/constant/CookieConstant.java
@@ -0,0 +1,11 @@
+package com.imooc.sell.constant;
+
+/**
+ * Created by Citrix on 2018/5/15.
+ */
+public interface CookieConstant {
+
+    String TOKEN = "token";
+
+    Integer EXPIRE = 7200;
+}
---
+++ src/main/java/com/imooc/sell/constant/RedisConstant.java
@@ -0,0 +1,11 @@
+package com.imooc.sell.constant;
+
+/**
+ * Created by Citrix on 2018/5/15.
+ */
+public interface RedisConstant {
+
+    String TOKEN_PREFIX = "token_%s";
+
+    Integer EXPIRE = 7200; //过期时间 2小时
+}
